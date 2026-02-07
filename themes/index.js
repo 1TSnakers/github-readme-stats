7fd95b70a49fafab6b23b1b92d288aef51f50dcd
Thomas McNamee <162380893+1TSnakers@users.noreply.github.com>
2026-02-07 11:18:33 -0500

    Update colors for streamlit_dark theme

--- themes/index.js
+++ themes/index.js
@@ -457,11 +457,11 @@ export const themes = {
     bg_color: "2b2d40",
   },
    streamlit_dark: {
-    title_color: "2f80ed",
+    title_color: "f63366",
     icon_color: "4c71f2",
-    text_color: "fffd80",
-    bg_color: "f0f2f6",
-    border_color: "f63366,fffd80",
+    text_color: "f0f2f6",
+    bg_color: "262730",
+    border_color: "f63366,f0f2f6",
   },
   ambient_gradient: {
     title_color: "ffffff",
